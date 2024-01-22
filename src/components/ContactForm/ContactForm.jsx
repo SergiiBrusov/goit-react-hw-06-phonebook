add077a9b3f2bc3ba27df9c33ee73a600797ed97
SergiiBrusov <127529976+SergiiBrusov@users.noreply.github.com>
2024-01-22 12:12:25 +0100

    done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
 import css from './ContactForm.module.css';
 import { nanoid } from 'nanoid';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact } from '../redux/contactsSlice';
+import { contactsSelector } from '../redux/selectors';
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export function ContactForm({ addContact }) {
+export function ContactForm() {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const contacts = useSelector(contactsSelector);
+  const dispatch = useDispatch();
 
   const id = nanoid();
 
@@ -24,43 +31,63 @@ export function ContactForm({ addContact }) {
     }
   };
 
+  const addContacts = ({ name, number }) => {
+    const isExist = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (isExist) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    dispatch(addContact(newContact));
+  };
+
   const handleSubmit = evt => {
     evt.preventDefault();
-    addContact({ name, number });
+    addContacts({ name, number });
     setName('');
     setNumber('');
   };
 
   return (
-    <form onSubmit={handleSubmit} className={css.form}>
-      <label htmlFor={id}>Name</label>
-      <input
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+$"
-        minLength="3"
-        maxLength="16"
-        id={id}
-        value={name}
-        onChange={handleChange}
-        className={css.inputName}
-        required
-      />
-      <label htmlFor={id}>Number</label>
-      <input
-        type="tel"
-        name="number"
-        pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-        title="xxx-xx-xx"
-        id={id}
-        value={number}
-        onChange={handleChange}
-        className={css.inputName}
-        required
-      />
-      <button type="submit" className={css.buttonContacts}>
+    <Form onSubmit={handleSubmit} className={css.form}>
+      <Form.Group controlId={id}>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+$"
+          minLength="3"
+          maxLength="16"
+          value={name}
+          onChange={handleChange}
+          className={css.inputName}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId={id}>
+        <Form.Label>Number</Form.Label>
+        <Form.Control
+          type="tel"
+          name="number"
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+          title="xxx-xx-xx"
+          value={number}
+          onChange={handleChange}
+          className={css.inputName}
+          required
+        />
+      </Form.Group>
+
+      <Button type="submit" className={css.buttonContacts}>
         Add Contact
-      </button>
-    </form>
+      </Button>
+    </Form>
   );
 }
